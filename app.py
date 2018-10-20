799ab2a854e6f18880ca0210716fe30fd52556ca
Mumshad Mannambeth <mmumshad@gmail.com>
2018-10-20 15:56:22 +0800

    First Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+import socket
+import random
+import os
+import argparse
+import time
+
+app = Flask(__name__)
+
+# Get start delay from Environment variable
+DELAY_FROM_ENV = os.environ.get('APP_START_DELAY') or 0
+
+@app.route("/")
+def main():
+    # return 'Hello'
+    return render_template('hello.html', name=socket.gethostname(), color='lightgreen')
+
+if __name__ == "__main__":
+
+    if DELAY_FROM_ENV:
+        print("Warming Up Application. Will take {0} seconds to finish.".format(DELAY_FROM_ENV))
+        time.sleep(DELAY_FROM_ENV)
+
+    print("Application Ready!")
+    # Run Flask Application
+    app.run(host="0.0.0.0", port=8080)
